4c9ba704ad9259f7f55fb0939fafa492453a0796
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2019-12-04 17:36:49 -0500

    login

--- src/main/java/com/resource/api/controller/UserController.java
+++ src/main/java/com/resource/api/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,9 +28,15 @@ public class UserController {
 	public List<User> getUsers(){
 		return userService.findAll();
 	}
+	
 	@GetMapping("/users/{userid}")
 	public User getUser(@PathVariable(value="userid") Long userid) {
 		return userService.findById(userid);
+	}
+	
+	@GetMapping("/login")
+	public User login(@RequestParam("useremail") String useremail, @RequestParam("userpassword") String userpassword) {
+		return userService.login(useremail, userpassword);
 	}
 	
 	@PostMapping("/usuarios")
